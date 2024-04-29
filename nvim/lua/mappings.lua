4b7007e05c35caa9741e55251fbb7f00b6d704b3
Adam Hester <hester.adam.c@gmail.com>
2024-04-28 22:19:48 -0400

    first commit

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,25 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "window left"})
+map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "window right"})
+map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "window down"})
+map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "window up"})
+
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+map('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+end, {remap=true})
+map('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+end, {remap=true})
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- 
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "rust-analyzer",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require('rust-tools').setup(opts)
+    end
+  },
+  {
+    'saecki/crates.nvim',
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function()
+  --     local M = require "plugins.configs.cmp"
+  --     table.insert(M.sources, {name = "crates"})
+  --   end,
+  -- }
+
+
+  --
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css", "query",
+        "c", "rust", "helm"
+  		},
+  	},
+  },
+  {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {
+      keys = 'etovxqpdygfblzhckisuran'
+    }
+  }
+}
